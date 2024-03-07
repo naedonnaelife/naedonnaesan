49f9cd75a6932e6c71f4f8f93e02fe6cae0215e5
지민 <81950388+jjm6604@users.noreply.github.com>
2024-03-07 12:39:49 +0900

    create: 프론트 구조

---
+++ Front/src/App.tsx
@@ -0,0 +1,17 @@
+import tw, { styled } from 'twin.macro';
+
+// const TestNavbar = styled.div`
+//   ${tw`
+//   flex
+//   p-4
+//   bg-blue-900
+//   justify-end
+//   `}
+//   color : white;
+// `;
+
+function App() {
+  return <></>;
+}
+
+export default App;

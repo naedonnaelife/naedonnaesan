966c6c449dfc2041bb79d0fa84f7dc8b01f42693
woomigyeong <migyom222222@gmail.com>
2024-03-08 16:58:16 +0900

    feat: 모든 파일 controller 추가

---
+++ Back/src/main/java/com/example/be/demo/gu/controller/GuController.java
@@ -0,0 +1,11 @@
+package com.example.be.demo.gu.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class GuController {
+}

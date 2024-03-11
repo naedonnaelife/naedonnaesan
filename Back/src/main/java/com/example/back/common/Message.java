f89a15982ed1037bf343406c53316cedbc808066
TaeHo Yun <ice_98@naver.com>
2024-03-11 15:38:34 +0900

    feat: OAuth 로그인만 사용, JWT 토큰 발급,재발급 로직 구현, 인가 처리까지 완료

---
+++ Back/src/main/java/com/example/back/common/Message.java
@@ -0,0 +1,18 @@
+package com.example.back.common;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Message {
+
+    private HttpStatusEnum status;
+    private String message;
+    private Object object;
+
+    public Message(HttpStatusEnum status, String message, Object object) {
+        this.status = status;
+        this.message = message;
+        this.object = object;
+    }
+}

9ec82848a1f279b612c61b0b319b9940d1ef35fd
Munsoo Kang <dalsu1742@gmail.com>
2024-03-12 12:27:57 +0900

    feat:animtation

--- Front/src/components/entry/reuse/Card.tsx
+++ Front/src/components/entry/reuse/Card.tsx
@@ -1,6 +1,7 @@
 import tw, { styled } from 'twin.macro';
+import { keyframes } from 'styled-components';
 import cute from '../../../assets/test.png'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 interface CardProps {
     index : number
@@ -10,45 +11,30 @@ type heightProps = {
     height : string
 }
 
-const CardWrapper = styled.figure`
-  ${tw`flex absolute w-[80%] h-[500px] border-2 border-black p-2 my-40`}
-  ${({ height }: heightProps) => `top : ${height}px`}
+const ani = () => keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
 `
 
-const CardImage = styled.img<CardProps>`
-  ${tw`w-[50%] h-[100%] border-2 border-black object-fill`}
-  
-  ${({ height }:heightProps ) => `
-    @keyframes move {
-      from {
-        top: ${height + 400}px;
-        opacity: 0;
-      }
-      to {
-        top: ${height}px;
-        opacity: 1;
-      }
-    }
+const CardWrapper = styled.figure`
+  ${tw`flex relative w-[100%] border-2 border-green-500`}
+  ${({ height }:heightProps) => `height: ${height}px;`}
+`;
 
-    animation: move 2s;
-  `}
+const CardImage = styled.img<CardProps>`
+  ${tw` absolute w-[50%] h-full border-2 border-black object-fill`}
+  ${({ width }) => `left: ${width}px;`}
+  ${({ width }) => css`animation : ${ani(width)} 1s ease-in-out;`}
 `;
 
 const CaptionWrapper= styled.figcaption`
-    ${tw`flex flex-col justify-center items-center w-[50%]  border-2 border-red mx-2 `}
-    @keyframes move {
-    from {
-        top: 1500px;
-        opacity : 0;
-    }
-    to {
-        top: 0px;
-        opacity : 1;
-    }
-  }
-
-  animation: move 4s;
-
+    ${tw`flex-cc absolute w-[50%] h-full  border-2 border-red `}
+    ${({ width }) => `left: ${width}px;`}
+  animation : ${ani('1000px')} 2s linear ;
 `
 
 const Title = styled.h2`
@@ -60,18 +46,22 @@ const Imformation = styled.p`
 `
 
 const ServiceLink = styled.button`
-    ${tw`bg-kakaoBlue rounded-lg text-white p-2`}
+    ${tw`bg-kakaoBlue rounded-lg text-white`}
 `
 
 const  Card:React.FC<CardProps> = ({index}) => {
-    const height = `${window.innerHeight * (index + 1 ) / 2 + 400}`
+    const [animate, setAnimate] = useState(false)
+    const height = `${window.innerHeight}`
+    const width = `${window.innerWidth / 2}`
 
     const handleScroll = () => {
 
-    const height = window.scrollY;
-    console.log('스크롤 높이 : ', height, '화면 전체 높이 : ', window.innerHeight)    
-        if (height < 200 * window.innerHeight) {
-          console.log('hi');
+    const scrollY = window.scrollY;
+    console.log('스크롤 높이 : ', scrollY, '화면 전체 높이 : ', window.innerHeight, '인덱스 : ', index)    
+        if (scrollY < 200 * window.innerHeight) {
+          setAnimate(true)
+        } else {
+          setAnimate(false)
         }
       };
 
@@ -86,19 +76,20 @@ const  Card:React.FC<CardProps> = ({index}) => {
     return (
     <>
         {(index % 2) === 0 ? 
-        <CardWrapper height={height}>
-            <CardImage src={cute} alt='image' />
+        <CardWrapper height={height} animate={animate}>
+            <CardImage width={width} src={cute} alt='image' />
             <CaptionWrapper>
                 <Title>동네 매물 보기</Title>
-                <Imformation>우리팀 너무 든든하다 박박박과 장선생님 난 뒤에서 tailwind.config나 깔짝이고 있을게... 우리팀 화이팅!! </Imformation>
+                <Imformation> 동네 매물 보기 정보입니다 </Imformation>
                 <ServiceLink>A서비스 바로가기</ServiceLink>
             </CaptionWrapper>
         </CardWrapper>
         :
-        <CardWrapper>
-            <CaptionWrapper>
+
+        <CardWrapper height={height}>
+            <CaptionWrapper width={width}>
                 <Title>동네 매물 보기</Title>
-                <Imformation>우리팀 너무 든든하다 박박박과 장선생님 난 뒤에서 tailwind.config나 깔짝이고 있을게... 우리팀 화이팅!! </Imformation>
+                <Imformation> 동네 매물 보기 정보입니다</Imformation>
                 <ServiceLink>A서비스 바로가기</ServiceLink>
             </CaptionWrapper>
             <CardImage src={cute} alt='image' />
--- Front/tailwind.config.js
+++ Front/tailwind.config.js
@@ -39,5 +39,6 @@ export default {
       };
       addUtilities(newUtilities);
     },
+    
   ],
 };

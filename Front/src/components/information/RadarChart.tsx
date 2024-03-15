c2140f9dccac93c3e4112cac6e2f63215d2fa974
지민 <81950388+jjm6604@users.noreply.github.com>
2024-03-15 17:03:28 +0900

    Feat: 테스트 데이터

--- Front/src/components/information/RadarChart.tsx
+++ Front/src/components/information/RadarChart.tsx
@@ -6,36 +6,33 @@ import { Radar } from 'react-chartjs-2';
 const ChartWrapper = styled.figure`
   ${tw`flex justify-center h-[50%] border-basic m-1`}
 `;
-
-const RadarChart: React.FC = () => {
-  // {
-  //   "safety" : 3,
-  //   "leisure" : 3,
-  //   "welfare" : 3,
-  //   "transp" : 3,
-  //   "food" : 3,
-  //   "convenience" : 3
-  // }
+interface RadarProps {
+  dongScore: number[];
+  seoulScore: number[];
+  labels: string[];
+}
+const RadarChart: React.FC<RadarProps> = ({ dongScore, seoulScore, labels }) => {
   ChartJS.register(RadialLinearScale, PointElement, LineElement, Filler, Tooltip, Legend);
   const chartData = {
-    labels: ['안전', '여가', '보건', '교통', '음식점', '편의시설'],
+    labels: labels,
     datasets: [
       {
         label: '내가 선택한 동',
-        data: [2, 6, 7, 8, 6, 3],
+        data: dongScore,
         backgroundColor: 'rgba(255, 99, 132, 0.2)',
         borderColor: 'rgba(255, 99, 132, 1)',
         borderWidth: 1,
       },
       {
         label: '서울시 평균',
-        data: [5, 5, 5, 5, 5, 7],
+        data: seoulScore,
         backgroundColor: 'rgba(100, 99, 100 , 0.2)',
         borderColor: 'rgba(100, 99, 100, 1)',
         borderWidth: 1,
       },
     ],
   };
+  console.log(dongScore, seoulScore, labels);
   return (
     <ChartWrapper>
       <Radar data={chartData} />

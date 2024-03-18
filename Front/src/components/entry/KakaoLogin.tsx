3b8eff2e8f8f7d736963ca32d243672018e83b0f
Munsoo Kang <dalsu1742@gmail.com>
2024-03-18 10:20:39 +0900

    fix:api_key

--- Front/src/components/entry/KakaoLogin.tsx
+++ Front/src/components/entry/KakaoLogin.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import tw, { styled } from 'twin.macro';
 import UseAxios from '../../utils/UseAxios';
+import axios from 'axios';
 
 
 const LoginButton = styled.button`
@@ -11,11 +12,12 @@ const KakaoLogin:React.FC = () => {
     const [code, setCode] = useState<any>(null);
     const customAxios = UseAxios()
     const redirect_uri = 'http://localhost:5173';
+    const REST_API_KEY = '218aa28a9e8fa4d947c106cb95b2ec1b';
     // const REST_API_KEY = process.env.REACT_APP_REST_API_K
-    // const REST_API_KEY = 'ce5dc2c94a97f41bed0f25224d3e3587';
-    const REST_API_KEY = '13e2daef357ed33894d3c75e4bb31528';
+    // const REST_API_KEY = '13e2daef357ed33894d3c75e4bb31528';
     const kakaoURL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${redirect_uri}&prompt=login`;
-  
+
+
     const kakaoLogin = () => {
         window.location.href = kakaoURL;
       }
@@ -23,11 +25,17 @@ const KakaoLogin:React.FC = () => {
     const getToken = async (code:any) => {
       try {
         // 백엔드로 인가 코드를 전송하여 데이터 요청
-        const response = await customAxios.get(`/oauth`, {
+        console.log('code 확인 : ', code)
+        const response = await axios.get(`http://localhost:8080/api/oauth`, {
           params: {
             code: code,
           },
+        // const response = await customAxios.get(`/oauth`, {
+        //   params: {
+        //     code: code,
+        //   },
         });
+        console.log('응답 : ', response)
         localStorage.setItem("accessToken", response.headers['authorization']);
         localStorage.setItem("refreshToken", response.headers['authorization-refresh']);
         localStorage.setItem("kakaoToken", response.headers['kakao-authorization']);
@@ -37,6 +45,7 @@ const KakaoLogin:React.FC = () => {
     };
   
     useEffect(() => {
+
         // 페이지 로딩 시 인가 코드 추출
         const extractedCode = new URL(window.location.href).searchParams.get(
           'code',
@@ -46,6 +55,7 @@ const KakaoLogin:React.FC = () => {
         // 코드가 있을 경우 자동으로 데이터 요청
         if (extractedCode) {
           console.log('발급받은 코드 : ', extractedCode)
+            // getToken(code);
             getToken(extractedCode);
         }
       }, []);

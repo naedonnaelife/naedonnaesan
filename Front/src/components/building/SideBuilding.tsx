bff818a515ba04cdc0ebcf8bc9f4d4de79731029
지민 <81950388+jjm6604@users.noreply.github.com>
2024-03-18 23:20:59 +0900

    Feat: 매물 목록

--- Front/src/components/building/SideBuilding.tsx
+++ Front/src/components/building/SideBuilding.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import tw, { styled } from 'twin.macro';
 import BuildingCard from './reuse/BuildingCard.tsx';
 import SearchBar from '../../utils/SearchBar.tsx';
 
+type buildingOpenProps = {
+  isBuildingOpen : boolean
+}
+
 const Aside = styled.aside`
   ${tw`w-[25%] h-[100%] border-r-2 border-lightGray drop-shadow-lg p-2 bg-white
-  max-sm:w-[100%] max-sm:absolute max-sm:-bottom-[90%] z-10`}
+  max-sm:w-[100%] max-sm:absolute z-10`}
+  ${({ isBuildingOpen } : buildingOpenProps) => (isBuildingOpen ? tw`max-sm:-bottom-[0%]` : tw`max-sm:-bottom-[90%]`)}
 `;
 const Card = styled.article`
   ${tw`flex w-[100%] h-[15%] p-1`}
@@ -23,9 +28,13 @@ const Button = styled.button`
 
 const testList = [1, 2, 3, 4, 5];
 const SideBuilding: React.FC = () => {
+  const [isBuildingOpen, setIsBuildingOpen] = useState(false)
+  const handleHamburgerButton = () => {
+    setIsBuildingOpen(prev => !prev)
+  }
   return (
-    <Aside>
-      <HamburgerButton>버튼</HamburgerButton>
+    <Aside isBuildingOpen={isBuildingOpen}>
+      <HamburgerButton onClick={handleHamburgerButton}>버튼</HamburgerButton>
       <SearchBar />
       <ButtonWrapper>
         <Button>전/월세</Button>

5ebb71d0b11e24bf79df1c9c2c0c3b73ffef22a1
TaeHo Yun <ice_98@naver.com>
2024-03-18 10:11:27 +0900

    feat: article 엔티티 생성

---
+++ Back/src/main/java/com/example/back/article/entity/Article.java
@@ -0,0 +1,30 @@
+package com.example.back.article.entity;
+
+import com.example.back.building.BuildingType;
+import com.example.back.building.PayType;
+import com.example.back.dong.entity.Dong;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "article_id")
+    private Long articleId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "dong_id")
+    private Dong dong;
+
+    private String title; // 제목은 보통 길지 않으므로 VARCHAR로 충분함
+
+    @Lob
+    private String content; // 본문은 길 수 있으므로 @Lob 사용
+
+    @Column(length = 2048)
+    private String imageUrl;
+}
+

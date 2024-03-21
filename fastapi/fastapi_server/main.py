a9fe7593ec64010c96bc15aa11302a7d8d46e24e
Jo-dv <whgjsgh11@gmail.com>
2024-03-21 17:22:48 +0900

    test: model modify
    Former-commit-id: 8f7f66f1f3b8cb643e67861de307ab480f6bf366

--- fastapi/fastapi_server/main.py
+++ fastapi/fastapi_server/main.py
@@ -2,15 +2,15 @@ import joblib
 import numpy as np
 from fastapi import FastAPI
 from pydantic import BaseModel
-from sklearn.decomposition import PCA
 
 app = FastAPI()
 
 # 모델 파일이 있는 절대 경로 설정
-model_path = '/code/app/model.joblib'  # 예시 경로, 실제 경로로 수정해야 함
+model_path = '/code/app/'  # 예시 경로, 실제 경로로 수정해야 함
 
 # 모델 로드
-model = joblib.load(model_path)
+pca_model = joblib.load(model_path + "pca_model.joblib")
+knn_model = joblib.load(model_path + "knn_model.joblib")
 
 
 class PredictRequest(BaseModel):
@@ -24,11 +24,10 @@ async def predict(request: PredictRequest):
     data = np.array(request.features).reshape(1, -1)
     print("np data: ", data)
     # 입력 데이터에 PCA 적용
-    pca = PCA(n_components=2, random_state=512)
-    reduced_data = pca.transform(data)
+    reduced_data = pca_model.transform(data)
     print("pca data: ", reduced_data)
     # 모델 예측
-    prediction = model.predict(reduced_data)
+    prediction = knn_model.predict(reduced_data)
     return {"prediction": int(prediction[0])}
 
 

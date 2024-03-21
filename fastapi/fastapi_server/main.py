eb55f336b1a082ede0c89fd4866e510d61a74699
Jo-dv <63555689+Jo-dv@users.noreply.github.com>
2024-03-21 20:21:53 +0900

    test: ai api
    Former-commit-id: e8070fdcbf3ca98cc3ab66aff8f25670a00691ef

--- fastapi/fastapi_server/main.py
+++ fastapi/fastapi_server/main.py
@@ -1,12 +1,14 @@
 import joblib
 import numpy as np
+import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel
+from sklearn.neighbors import NearestNeighbors
 
 app = FastAPI()
 
 # 모델 파일이 있는 절대 경로 설정
-model_path = '/code/app/'  # 예시 경로, 실제 경로로 수정해야 함
+model_path = '/code/app/'  # 컨테이너 내 경로
 
 # 모델 로드
 pca_model = joblib.load(model_path + "pca_model.joblib")
@@ -18,17 +20,33 @@ class PredictRequest(BaseModel):
 
 
 @app.post("/predict")
-async def predict(request: PredictRequest):
-    # 입력 데이터를 numpy 배열로 변환
-    print("input: ", request.features)
-    data = np.array(request.features).reshape(1, -1)
-    print("np data: ", data)
+async def predict(preference):
+    # 클러스터 생성
+    df = pd.read_csv("cluster.csv", index_col=0)
+    df_train = df.drop(axis=1, columns=['법정동', '군집'])
+    pc = pca_model.fit_transform(df_train)
+    cluster = pd.DataFrame(pc)
+    cluster['target'] = df['군집']
+
+    # 입력 데이터 numpy 배열로 변환
+    input_data = np.array(preference.features).reshape(1, -1)
+
     # 입력 데이터에 PCA 적용
-    reduced_data = pca_model.transform(data)
-    print("pca data: ", reduced_data)
-    # 모델 예측
-    prediction = knn_model.predict(reduced_data)
-    return {"prediction": int(prediction[0])}
+    reduced_data = pca_model.transform(input_data)
+
+    # 타겟 모델 예측
+    predict_data = knn_model.predict(reduced_data)
+
+    # 추천
+    filtered_data = cluster[cluster['target'] == predict_data[0]]
+
+    neigh = NearestNeighbors(n_neighbors=3)
+    neigh.fit(filtered_data.drop(axis=1, columns=['target']))
+    distances, indices = neigh.kneighbors(reduced_data)
+
+    closest_points = filtered_data.iloc[indices[0]]
+    print(df.iloc[closest_points.index])
+    return {"recommend": closest_points}
 
 
 @app.get("/")

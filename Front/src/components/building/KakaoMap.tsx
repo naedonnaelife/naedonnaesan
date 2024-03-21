23133f51af18f1ad3a47b5c4e7e9ef8b4d9b26e2
지민 <81950388+jjm6604@users.noreply.github.com>
2024-03-21 19:42:46 +0900

    Feat: 매물 페이지

--- Front/src/components/building/KakaoMap.tsx
+++ Front/src/components/building/KakaoMap.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import tw, { styled } from 'twin.macro';
 import newDong from '../../datas/newDong.json';
 import test from '../../datas/test.json';
+import UseAxios from '../../utils/UseAxios.tsx';
 
 interface KakaoMapProps {
   areaName: string;
@@ -17,6 +18,7 @@ const Map = styled.div`
 const { kakao } = window;
 
 const KakaoMap: React.FC<KakaoMapProps> = ({ areaName }) => {
+  const axios = UseAxios();
   const selectedDong: any = newDong.features.find((dong: any) => dong.properties.temp === areaName);
   const x = selectedDong.properties.x;
   const y = selectedDong.properties.y;
@@ -56,14 +58,14 @@ const KakaoMap: React.FC<KakaoMapProps> = ({ areaName }) => {
     const clusterer = new kakao.maps.MarkerClusterer({
       map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
       averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
-      minLevel: 2, // 클러스터 할 최소 지도 레벨
-      calculator: [10, 30, 50], // 클러스터의 크기 구분 값, 각 사이값마다 설정된 text나 style이 적용된다
-      texts: ['10+', '30+', '50+'],
+      minLevel: 1, // 클러스터 할 최소 지도 레벨
+      calculator: [2, 3, 5, 10, 30, 50, 100, 300, 500, 1000], // 클러스터의 크기 구분 값, 각 사이값마다 설정된 text나 style이 적용된다
+      texts: ['2+','3+', '5+', '10+', '30+', '50+', '100+', '300+', '500+', '1000+'],
       styles: [
         {
           // calculator 각 사이 값 마다 적용될 스타일을 지정한다
-          width: '40px',
-          height: '40px',
+          width: '30px',
+          height: '30px',
           background: `url("${imageSrc}") round`,
           color: '#fff', // 글자색
           // opacity: '0.7',
@@ -71,8 +73,23 @@ const KakaoMap: React.FC<KakaoMapProps> = ({ areaName }) => {
           // borderRadius: '100px',
           textAlign: 'center',
           fontWeight: 'bold',
-          lineHeight: '41px',
+          lineHeight: '31px',
           paddingTop: '8px',
+        },
+        {
+          width: '35px',
+          height: '35px',
+          lineHeight: '36px',
+        },
+        {
+          width: '40px',
+          height: '40px',
+          lineHeight: '41px',
+        },
+        {
+          width: '45px',
+          height: '45px',
+          lineHeight: '46px',
         },
         {
           width: '50px',
@@ -84,21 +101,48 @@ const KakaoMap: React.FC<KakaoMapProps> = ({ areaName }) => {
           height: '60px',
           lineHeight: '61px',
         },
+        {
+          width: '70px',
+          height: '70px',
+          lineHeight: '71px',
+        },
+        {
+          width: '70px',
+          height: '70px',
+          lineHeight: '71px',
+        },
+        {
+          width: '70px',
+          height: '70px',
+          lineHeight: '71px',
+        },
+        {
+          width: '70px',
+          height: '70px',
+          lineHeight: '71px',
+        },
       ],
     });
 
     // 데이터에서 좌표 값을 가지고 마커를 표시합니다
     // 마커 클러스터러로 관리할 마커 객체는 생성할 때 지도 객체를 설정하지 않습니다
-    const markers = test.positions.map((position: { lat: number; lng: number }) => {
-      return new kakao.maps.Marker({
-        position: new kakao.maps.LatLng(position.lat, position.lng),
-        image: markerImage,
+    axios.get('/buildings', { params : {dongname: '신사동'}})
+    .then((response)=>{
+      console.log(response.data)
+      const markers = response.data.object.map((position: { x: number; y: number }) => {
+        return new kakao.maps.Marker({
+          position: new kakao.maps.LatLng(position.x, position.y),
+          image: markerImage,
+        });
       });
-    });
-    console.log(markers);
-    // 클러스터러에 마커들을 추가합니다
-    clusterer.addMarkers(markers);
-    console.log(clusterer.getStyles());
+      console.log(markers);
+      clusterer.addMarkers(markers);
+    })
+    .catch((error)=>{
+    console.log(error)
+    })
+
+    
   }, []);
 
   return (
--- Front/src/components/building/SideBuilding.tsx
+++ Front/src/components/building/SideBuilding.tsx
@@ -1,20 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import tw, { styled } from 'twin.macro';
+import { useInView } from 'react-intersection-observer';
 import BuildingCard from './reuse/BuildingCard.tsx';
 import SearchBar from '../../utils/SearchBar.tsx';
+import UseAxios from '../../utils/UseAxios.tsx';
 
-type buildingOpenProps = {
+type Building = {
+  buildingId: string;
+  payType: string;
+  deposit: number;
+  monthlyPay: number;
+  name: string;
+  buildingType: string;
+  floor: number;
+  area: number;
+  address: string;
+  x: string;
+  y: string;
+}
+
+type StyleProps = {
   isBuildingOpen: boolean;
 };
 
 const Aside = styled.aside`
-  ${tw`w-[25%] h-[100%] border-r-2 border-lightGray drop-shadow-lg p-2 bg-white duration-200
+  ${tw`w-[25%] h-[100%] border-r-2 border-lightGray drop-shadow-lg px-2 bg-white duration-200 overflow-y-auto
     max-sm:absolute max-sm:z-10 max-sm:w-[100%] `}
-  ${({ isBuildingOpen }: buildingOpenProps) => (isBuildingOpen ? tw`max-sm:-bottom-[0%]` : tw`max-sm:-bottom-[90%]`)}
+  ${({ isBuildingOpen }: StyleProps) => (isBuildingOpen ? tw`max-sm:-bottom-[0%]` : tw`max-sm:-bottom-[90%]`)}
 `;
 const Card = styled.article`
   ${tw`flex w-[100%] h-[15%] p-1`}
 `;
+const SideFixWrapper = styled.div`
+  ${tw`w-[100%] sticky top-0 bg-white`}
+`
 const ButtonWrapper = styled.aside`
   ${tw`flex justify-between w-[100%]`}
 `;
@@ -22,30 +41,75 @@ const HamburgerButton = styled.button`
   ${tw`hidden w-[100%] h-[10%]
     max-sm:flex-c`}
 `;
+const SelectedCard = styled.article`
+${tw`flex w-[100%] h-[15%] p-1`}
+`;
 const Button = styled.button`
   ${tw`bg-dongButton rounded-3xl px-4 py-1 my-2`}
 `;
+const ScrollDiv = styled.div`
+  ${tw`bg-red`}
+`
 
-const testList = [1, 2, 3, 4, 5];
 const SideBuilding: React.FC = () => {
+  const [testList, setTestList] = useState([1, 2, 3, 4, 5]);
   const [isBuildingOpen, setIsBuildingOpen] = useState(false);
+  const [page, setPage] = useState(0);
+  const [buildingList, setBuildingList] = useState<Building | []>([]);
+  const [isLast, setIsLast] = useState(false);
+  const [pageRef, inView] = useInView();
+  const axios = UseAxios();
+
   const handleHamburgerButton = () => {
     setIsBuildingOpen((prev) => !prev);
   };
+  const getBuildingList = () => {
+    axios.get('/buildings/name', { params : {dongname: '신사동', page: page}})
+    .then((response)=>{
+      console.log(response.data)
+      setPage(prev => prev + 1)
+      // setBuildingList([...buildingList, response.data.object.buildingDtoList])
+      setIsLast(response.data.object.last)
+  })
+  .catch((error)=>{
+  console.log(error)
+  })
+  
+  }
+  
+  useEffect(() => {
+    // getBuildingList()
+    console.log('여기서 처음 매물 리스트 받아옴!')
+  }, [])
+  
+  useEffect(()=> {
+    if(inView && !isLast){
+    // getBuildingList()
+    console.log('여기서 무한 스크롤 구현!')
+    setTestList([...testList, 1, 2, 3, 4, 5])
+  }
+  }, [inView])
+
   return (
     <Aside isBuildingOpen={isBuildingOpen}>
+      <SideFixWrapper>
+
       <HamburgerButton onClick={handleHamburgerButton}>버튼</HamburgerButton>
       <SearchBar />
       <ButtonWrapper>
-        <Button>전/월세</Button>
         <Button>가격</Button>
         <Button>유형</Button>
       </ButtonWrapper>
+      <SelectedCard>
+        <BuildingCard />
+      </SelectedCard>
+      </SideFixWrapper>
       {testList.map((test) => (
         <Card>
           <BuildingCard key={test}></BuildingCard>
         </Card>
       ))}
+      <ScrollDiv ref={pageRef} />
     </Aside>
   );
 };

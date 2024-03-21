125a8324db7b025611add9986aed3355c9e51ad1
Taeho <ice_98@naver.com>
2024-03-22 00:45:58 +0900

    fix: 엔티티 변경 완료

--- Back/src/main/java/com/example/back/convenience/repository/ConvenienceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.back.convenience.repository;
-
-
-import com.example.back.convenience.entity.Convenience;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConvenienceRepository extends JpaRepository<Convenience, Long> {
-}

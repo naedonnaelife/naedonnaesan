d430c5d905c9c08ca46f05792979b6616b009a08
TaeHo Yun <ice_98@naver.com>
2024-03-25 17:42:05 +0900

    feat:BERTopic으로 모델 훈련중에 안됨
    Former-commit-id: 2a5ff31da8b7cba24e8f027717dc1e023ad815ab

--- fastapi/fastapi_server/main.py
+++ fastapi/fastapi_server/main.py
@@ -1,12 +1,17 @@
 import joblib
 import numpy as np
 import pandas as pd
+import time
 from fastapi import FastAPI
 from pydantic import BaseModel
 from sklearn.neighbors import NearestNeighbors
 from pymongo import MongoClient
 from datetime import datetime, timedelta
 from bson import ObjectId
+from konlpy.tag import Okt
+from collections import Counter
+from bertopic import BERTopic
+from typing import List
 
 # MongoDB 접속 정보
 user = "mango"
@@ -28,23 +33,9 @@ collection = db["news"]  # 사용할 컬렉션 이름
 
 # documents = collection.find()  # 컬렉션 내 모든 문서 조회
 
-# 오늘 날짜와 정확히 1년 전 날짜 계산
-today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-one_year_ago = today - timedelta(days=366)
-
-# 날짜를 "YYYY-MM-DD" 형식의 문자열로 변환
-one_year_ago_str = one_year_ago.strftime("%Y-%m-%d")
-# 문자열 기반으로 1년 전 날짜에 해당하는 기사 조회
-last_year_news = collection.find({"published": {"$regex": f"^{one_year_ago_str}"}})
-last_year_news = list(last_year_news)
-
-print(len(last_year_news))
-print(one_year_ago)
-
 with open('korean_stopwords.txt', 'r', encoding='utf-8') as f:
   list_file = f.readlines() 
 stopwords = list_file[0].split(",")
-print(list_file)
 app = FastAPI()
 
 # 모델 파일이 있는 절대 경로 설정
@@ -94,13 +85,59 @@ async def predict(preference: PredictRequest):
 
     return recommend.to_dict()
 
-@app.post("/")
-def test():
-    return ""
 
-@app.get("/")
-def health_check():
-    return 200
+@app.get("/ai/keyword")
+def getKeyword():
+
+    # 오늘 날짜와 정확히 1년 전 날짜 계산
+    today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    one_year_ago = today - timedelta(days=366)
+
+    # 날짜를 "YYYY-MM-DD" 형식의 문자열로 변환
+    one_year_ago_str = one_year_ago.strftime("%Y-%m-%d")
+    # 문자열 기반으로 1년 전 날짜에 해당하는 기사 조회를 위한 쿼리 준비
+    query = {"published": {"$regex": f"^{one_year_ago_str}"}}
+
+    # 쿼리 실행 전 시간 측정
+    start_time = time.time()
+
+    ## published 필드 기준으로 인덱싱
+    collection.create_index([("published", 1)])
+    # 문자열 기반으로 1년 전 날짜에 해당하는 기사 조회
+    last_year_news = collection.find({"published": {"$regex": f"^{one_year_ago_str}"}})
+
+    # 쿼리 결과를 리스트로 변환하여 길이 측정
+    # 주의: 대량의 데이터를 처리할 경우, 메모리 문제를 유발할 수 있습니다.
+    last_year_news = list(last_year_news)
+
+    # 쿼리 실행 후 시간 측정
+    end_time = time.time()
+
+    # 실행 시간 출력
+    print(f"쿼리 실행 시간: {end_time - start_time}초")
+    # print(last_year_news[0]['article'])
+    # 뉴스 기사 본문 추출
+    documents = [news['article'] for news in last_year_news if 'article' in news]
+
+    # BERTopic 모델 훈련
+    topic_model = BERTopic()
+    topics, _ = topic_model.fit_transform(documents)
+
+    # 가장 대표적인 토픽 3개 추출
+    topic_freq = topic_model.get_topic_info()  # 토픽과 빈도수 정보를 가져옴
+    top_3_topics = topic_freq.head(4)  # 상위 3개 토픽 선택 (첫 번째 행은 전체 문서를 나타냄)
+
+    # 각 토픽의 상위 단어 추출 및 결과 준비
+    top_3_topic_words = []
+    for topic_num in top_3_topics['Topic'][1:]:  # 첫 번째 행(전체 문서)는 제외
+        topic_words = topic_model.get_topic(topic_num)
+        top_3_topic_words.append({
+            "topic": topic_num,
+            "words": [word[0] for word in topic_words[:5]],  # 상위 5개 단어만 선택
+            "coherence": topic_words[0][1]  # 첫 번째 단어의 점수(일관성)를 토픽의 점수로 사용
+        })
+
+    return top_3_topic_words
 
 
 @app.get("/status")

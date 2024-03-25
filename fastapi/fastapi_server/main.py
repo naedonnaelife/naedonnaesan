ce8f71fb0dd0e632a33d66ad62b5a18e120035f8
TaeHo Yun <ice_98@naver.com>
2024-03-25 13:48:59 +0900

    feat: 1년전 뉴스기사 가져오기 완료
    Former-commit-id: 6888c29c1c15b32d846554ed093585e7e46e5d64

--- fastapi/fastapi_server/main.py
+++ fastapi/fastapi_server/main.py
@@ -4,15 +4,57 @@ import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel
 from sklearn.neighbors import NearestNeighbors
+from pymongo import MongoClient
+from datetime import datetime, timedelta
+from bson import ObjectId
 
+# MongoDB 접속 정보
+user = "mango"
+password = "sweetmango123"
+host = "j10e204.p.ssafy.io"  # 또는 MongoDB가 실행되고 있는 서버의 주소
+port = 27017
+dbname = "mango"  # 접속하고자 하는 데이터베이스 이름
+
+# MongoDB URI 구성
+uri = f"mongodb://{user}:{password}@{host}:{port}"
+
+# MongoDB 클라이언트 생성
+client = MongoClient(uri)
+
+# 데이터베이스 선택
+db = client[dbname]
+# 접속 테스트를 위한 컬렉션 및 데이터 출력 예시
+collection = db["news"]  # 사용할 컬렉션 이름
+
+# documents = collection.find()  # 컬렉션 내 모든 문서 조회
+
+# 오늘 날짜와 정확히 1년 전 날짜 계산
+today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+one_year_ago = today - timedelta(days=366)
+
+# 날짜를 "YYYY-MM-DD" 형식의 문자열로 변환
+one_year_ago_str = one_year_ago.strftime("%Y-%m-%d")
+# 문자열 기반으로 1년 전 날짜에 해당하는 기사 조회
+last_year_news = collection.find({"published": {"$regex": f"^{one_year_ago_str}"}})
+last_year_news = list(last_year_news)
+
+print(len(last_year_news))
+print(one_year_ago)
+
+with open('korean_stopwords.txt', 'r', encoding='utf-8') as f:
+  list_file = f.readlines() 
+stopwords = list_file[0].split(",")
+print(list_file)
 app = FastAPI()
 
 # 모델 파일이 있는 절대 경로 설정
 model_path = '/code/app/'  # 컨테이너 내 경로
 
 # 모델 로드
-pca_model = joblib.load(model_path + "pca_model.joblib")
-knn_model = joblib.load(model_path + "knn_model.joblib")
+# pca_model = joblib.load(model_path + "pca_model.joblib")
+# knn_model = joblib.load(model_path + "knn_model.joblib")
+pca_model = joblib.load("pca_model.joblib")
+knn_model = joblib.load("knn_model.joblib")
 
 
 class PredictRequest(BaseModel):
@@ -52,6 +94,9 @@ async def predict(preference: PredictRequest):
 
     return recommend.to_dict()
 
+@app.post("/")
+def test():
+    return ""
 
 @app.get("/")
 def health_check():

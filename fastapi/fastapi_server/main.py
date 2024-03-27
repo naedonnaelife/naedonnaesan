3d486c293af909b9f764ce597ab23a3768100063
TaeHo Yun <ice_98@naver.com>
2024-03-27 15:02:00 +0900

    fix: 뉴스 기사 키워드 추출 완료 (스케줄링 걸어서 매일 자정에 함수 실행되게)
    Former-commit-id: afcfd64d5be64d06a5e34c1e605869ba003d6d09

--- fastapi/fastapi_server/main.py
+++ fastapi/fastapi_server/main.py
@@ -1,17 +1,21 @@
-from datetime import datetime, timedelta
-
 import httpx
 import joblib
 import numpy as np
 import pandas as pd
-import time
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from pymongo import MongoClient
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neighbors import NearestNeighbors
 from starlette.middleware.cors import CORSMiddleware
+# 여기 밑의 import는 뉴스기사용
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
+import time
+from pymongo import MongoClient
+from datetime import datetime, timedelta
 from konlpy.tag import Okt
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+from collections import OrderedDict
 
 # 모델 파일이 있는 절대 경로 설정
 file_path = '/code/app/'  # 컨테이너 내 경로
@@ -34,23 +38,24 @@ client = MongoClient(uri)
 db = client[dbname]
 # 접속 테스트를 위한 컬렉션 및 데이터 출력 예시
 collection = db["news"]  # 사용할 컬렉션 이름
+daily_keywords = db['daily_keywords']  # 사용할 컬렉션 선택 또는 생성
 
 # documents = collection.find()  # 컬렉션 내 모든 문서 조회
 
 # 모델 로드
-pca_model = joblib.load(file_path + "pca_model.joblib")
-knn_model = joblib.load(file_path + "knn_model.joblib")
+# pca_model = joblib.load(file_path + "pca_model.joblib")
+# knn_model = joblib.load(file_path + "knn_model.joblib")
 
 
-# pca_model = joblib.load("pca_model.joblib")
-# knn_model = joblib.load("knn_model.joblib")
+pca_model = joblib.load("pca_model.joblib")
+knn_model = joblib.load("knn_model.joblib")
 
 origins = [
     "j10e204.p.ssafy.io",
 ]
 
 app = FastAPI()
-
+scheduler = AsyncIOScheduler() # 매일 자정에 기사의 키워드 추출하기
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -128,8 +133,6 @@ async def predict(preference: PredictRequest):
     await send_data_to_spring_boot(response, preference.token)
     return response
 
-
-@app.get("/ai/keyword")
 def getKeyword():
 
     # 오늘 날짜와 정확히 1년 전 날짜 계산
@@ -145,21 +148,83 @@ def getKeyword():
     ## published 필드 기준으로 인덱싱
     collection.create_index([("published", 1)])
     # 문자열 기반으로 1년 전 날짜에 해당하는 기사 조회
-    last_year_news = list(collection.find({"published": {"$regex": f"^{one_year_ago_str}"}}, {"article": 1, "_id": 0}))
-    print(len(last_year_news))
+    last_year_news = list(collection.find({"published": {"$regex": f"^{one_year_ago_str}"}}, {"title": 1, "_id": 0}))
+    titles = [news['title'] for news in last_year_news if 'title' in news]
     # 쿼리 실행 후 시간 측정
     end_time = time.time()
     # 실행 시간 출력
     print(f"쿼리 실행 시간: {end_time - start_time}초")
 
+    print("가져온 뉴스 기사 개수 : " + str(len(titles)) + "개")
+    # 뉴스 데이터의 'title' 필드만 추출하여 리스트 생성
+    
+    unique_titles = list(OrderedDict.fromkeys(titles))
+    len(unique_titles)
     okt = Okt()
 
-    # 뉴스 데이터의 'article' 필드만 추출하여 리스트 생성
-    articles = [news['article'] for news in last_year_news if 'article' in news]
-    print(len(articles))
+    # 불용어 처리를 위한 stopwords 불러오기
+    with open('korean_stopwords.txt', 'r', encoding='utf-8') as file:
+        # 파일 내용 읽기
+        content = file.read()
+    stopwords = content.strip().split('\n')
+    stopwords
 
-    return 200
+    # 뉴스 제목에서 불용어 제거
+    cleaned_titles = []
+    for title in unique_titles:
+        words = title.split()  # 공백 기준으로 단어 분리
+        filtered_words = [word for word in words if word not in stopwords]  # 불용어가 아닌 단어만 선택
+        cleaned_title = ' '.join(filtered_words)  # 정제된 단어들을 다시 문자열로 결합
+        cleaned_titles.append(cleaned_title)
 
+    # 형태소 분석기 초기화
+    okt = Okt()
+
+    # 명사만 추출하는 함수
+    def get_nouns(text):
+        nouns = okt.nouns(text)
+        return [noun for noun in nouns if len(noun) > 2]  # 2글자 명사 제외
+
+    # TfidfVectorizer 초기화 (명사만 추출하여 사용)
+    tfidf_vectorizer = TfidfVectorizer(tokenizer=get_nouns, min_df=10)  # min_df는 최소 문서 빈도, 필요에 따라 조정
+
+    # TF-IDF 계산
+    tfidf_matrix = tfidf_vectorizer.fit_transform(cleaned_titles)  # cleaned_articles는 전처리된 뉴스 본문 리스트
+
+    # 전체 문서에 대한 각 단어의 TF-IDF 점수 합산
+    scores_sum = np.sum(tfidf_matrix, axis=0)
+
+    # 배열 형태로 변환
+    scores_sum_array = np.array(scores_sum).flatten()
+
+    # 피처 이름 가져오기
+    feature_names = np.array(tfidf_vectorizer.get_feature_names_out())
+
+    # 점수에 따라 내림차순으로 정렬하고 상위 N개의 인덱스 가져오기
+    top_n_indices = np.argsort(scores_sum_array)[::-1][:3]  # 상위 10개 키워드
+
+    # 상위 N개 키워드와 점수 출력
+    result = []
+    print("Top N Keywords:")
+    for index in top_n_indices:
+        result.append(feature_names[index])
+
+
+    document = {
+        "date": today.strftime("%Y-%m-%d"),
+        "keywords": result
+    }
+
+    # # 문서를 MongoDB 컬렉션에 저장
+    daily_keywords.insert_one(document)
+    return result
+
+# 스케줄러 시작
+scheduler.add_job(
+    getKeyword,  # 실행할 함수
+    CronTrigger(hour=0, minute=0, timezone='Asia/Seoul')
+)
+scheduler.start()
 
 @app.get("/status")
 async def sayHello():

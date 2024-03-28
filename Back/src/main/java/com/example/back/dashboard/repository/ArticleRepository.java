0e0761f7e72134e673d358c51d867424fd4c2d94
Jo-dv <whgjsgh11@gmail.com>
2024-03-28 17:43:32 +0900

    feat: 뉴스 무한 스크롤

--- Back/src/main/java/com/example/back/dashboard/repository/ArticleRepository.java
+++ Back/src/main/java/com/example/back/dashboard/repository/ArticleRepository.java
@@ -1,5 +1,8 @@
 package com.example.back.dashboard.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -9,6 +12,6 @@ import com.example.back.dashboard.document.Article;
 
 
 public interface ArticleRepository extends MongoRepository<Article, String> {
-	public List<Article> findByTitleContaining(String keyword);
-	public Optional<Article> findById(String id);
+	Page<Article> findByTitleContaining(String keyword, Pageable page);
+	Optional<Article> findById(String id);
 }

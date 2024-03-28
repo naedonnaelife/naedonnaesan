293247b8fbc17b6913d77e8228c6c10c7c1e3357
woomigyeong <migyom222222@gmail.com>
2024-03-28 16:28:26 +0900

    fix: 단위 변경

--- Back/src/main/java/com/example/back/report/service/ReportService.java
+++ Back/src/main/java/com/example/back/report/service/ReportService.java
@@ -140,7 +140,7 @@ public class ReportService {
         double dist = Math.sin(deg2rad(lat1))* Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1))*Math.cos(deg2rad(lat2))*Math.cos(deg2rad(theta));
         dist = Math.acos(dist);
         dist = rad2deg(dist);
-        dist = dist*60*1.1515*1609.34;
+        dist = dist*60*1.1515*1.60934;
 
         return dist; // km단위
     }

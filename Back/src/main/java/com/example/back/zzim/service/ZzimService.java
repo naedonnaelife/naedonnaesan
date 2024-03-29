4388be8c29267199aa637f506ecb19adb1437135
TaeHo Yun <ice_98@naver.com>
2024-03-29 12:12:21 +0900

    feat: dongname으로 찜하기,찜삭제 완료

--- Back/src/main/java/com/example/back/zzim/service/ZzimService.java
+++ Back/src/main/java/com/example/back/zzim/service/ZzimService.java
@@ -50,9 +50,42 @@ public class ZzimService {
         return savedZzim.getZzimId();
     }
 
+    public Long saveZzim(String dongName) {
+        // 동(Dong) 조회
+        Dong dong = dongRepository.findByDongName(dongName)
+                .orElseThrow(() -> new DongNotFoundException(dongName));
+        User user = getUser();
+
+        // 사용자와 동에 해당하는 찜(Zzim) 조회
+        Optional<Zzim> existingZzim = zzimRepository.findByUserAndDong(user, dong);
+
+        // 이미 존재하는 경우 처리. 근데 뭘 하는게 좋을지 모르겠음
+        if (existingZzim.isPresent()) {
+//            return existingZzim.get().getZzimId();
+            throw new AlreadyZzimedException(existingZzim.get().getZzimId());
+        }
+
+        // 존재하지 않는 경우, 새 찜(Zzim) 생성 및 저장
+        Zzim newZzim = new Zzim(dong, user);
+        Zzim savedZzim = zzimRepository.save(newZzim);
+
+        // 새로 저장된 찜(Zzim)의 ID 반환
+        return savedZzim.getZzimId();
+    }
+
     public Long deleteZzim(Long dongId){
 
         Dong dong = dongRepository.findById(dongId).orElseThrow(() -> new DongNotFoundException(dongId));
+        User user = getUser();
+
+        Zzim zzim = zzimRepository.findByUserAndDong(user, dong).orElseThrow(() -> new ZzimNotFoundException(user.getUserId()));
+        zzimRepository.delete(zzim);
+
+        return zzim.getZzimId();
+    }
+    public Long deleteZzim(String dongName){
+
+        Dong dong = dongRepository.findByDongName(dongName).orElseThrow(() -> new DongNotFoundException(dongName));
         User user = getUser();
 
         Zzim zzim = zzimRepository.findByUserAndDong(user, dong).orElseThrow(() -> new ZzimNotFoundException(user.getUserId()));

1e84f51a0212861912508651950361ef9ce38d23
지민 <81950388+jjm6604@users.noreply.github.com>
2024-03-30 16:59:50 +0900

    Fix: build 에러 수정

--- Front/src/components/comparison/reuse/Card.tsx
+++ Front/src/components/comparison/reuse/Card.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import tw, { styled } from "twin.macro";
 import ComparisonGraph from "../ComparisonGraph";
 // import UseAxios from "../../../utils/UseAxios"; 

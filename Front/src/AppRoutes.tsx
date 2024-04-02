83d6674febe5453f1d40c306cb07b84efc2506d0
unknown <dalsu1742@gmail.com>
2024-04-03 00:10:25 +0900

    fix:Route

--- Front/src/AppRoutes.tsx
+++ Front/src/AppRoutes.tsx
@@ -6,22 +6,22 @@ import Comparison from './pages/ComparisonPage.tsx';
 import Recommend from './pages/RecommendPage.tsx';
 import Building from './pages/BuildingPage.tsx';
 import InitialForm from './pages/InitialFormPage.tsx';
-// import PrivateRoute from './PrivateRoute.tsx';
+import PrivateRoute from './PrivateRoute.tsx';
 
-function AppRoutes() {
-
+const AppRoutes:React.FC = () => {
 
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Entry />} />
-        <Route path="/my" element={<My />} />
-        {/* <Route path="/my" element={<PrivateRoute isLogin={access} component={<My />} />}></Route> */}
-        <Route path="/information" element={<Information />} />
-        <Route path="/comparison" element={<Comparison />} />
-        <Route path="/recommend" element={<Recommend />} />
-        <Route path="/building" element={<Building />} />
-        <Route path="/initial" element={<InitialForm />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/information" element={<Information />} />
+          <Route path="/comparison" element={<Comparison />} />
+          <Route path="/recommend" element={<Recommend />} />
+          <Route path="/building" element={<Building />} />
+          <Route path="/initial" element={<InitialForm />} />
+          <Route path="/my" element={<My />} />
+        </Route>
         {/* 라우트 여기에 추가하세용 */}
       </Routes>
     </Router>

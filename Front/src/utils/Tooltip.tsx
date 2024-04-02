09493c45f9622f7200cea72cca460822f01ed0b8
지민 <81950388+jjm6604@users.noreply.github.com>
2024-04-02 11:03:55 +0900

    Fix: 툴팁 글씨굵기 변경

--- Front/src/utils/Tooltip.tsx
+++ Front/src/utils/Tooltip.tsx
@@ -1,42 +1,35 @@
 import { useState } from 'react';
 import tw, { styled } from 'twin.macro';
 interface TooltipProps {
-    data : string
+  data: string;
 }
 
 const TooltipWrapper = styled.div`
-    ${tw`flex relative mx-1`}
-`
+  ${tw`flex relative mx-1`}
+`;
 
 const TooltipButton = styled.button`
-    ${tw` h-[2.5vh] w-[1.25vw] border-2 rounded-full text-10 hover:bg-mango hover:scale-105
+  ${tw` h-[2.5vh] w-[1.25vw] border-2 rounded-full text-10 hover:bg-mango hover:scale-105
     max-sm:h-[2vh] max-sm:w-[4vw]`}
-`
+`;
 
 const TooltopDetail = styled.p`
-    ${tw`absolute left-[1.5vw] -top-[4vh] bg-semiWhite border-2 border-black rounded-lg text-nowrap px-2`}
-`
+  ${tw`absolute left-[1.5vw] -top-[4vh] bg-semiWhite border-2 border-black rounded-lg text-nowrap px-2 font-jamsilLight`}
+`;
 
-
-const Tooltip:React.FC<TooltipProps> = ({data}) => {
-    const [isTooltipVisible, setIsTooltipVisible] = useState(false);
+const Tooltip: React.FC<TooltipProps> = ({ data }) => {
+  const [isTooltipVisible, setIsTooltipVisible] = useState(false);
 
   return (
     <>
-    <TooltipWrapper>
-        <TooltipButton onMouseEnter={() => setIsTooltipVisible(true)}
-        onMouseLeave={() => setIsTooltipVisible(false)}>
-            ?
+      <TooltipWrapper>
+        <TooltipButton onMouseEnter={() => setIsTooltipVisible(true)} onMouseLeave={() => setIsTooltipVisible(false)}>
+          ?
         </TooltipButton>
-        {isTooltipVisible && (
-        <TooltopDetail>
-            {data}
-        </TooltopDetail>)}
-    </TooltipWrapper>
+        {isTooltipVisible && <TooltopDetail>{data}</TooltopDetail>}
+      </TooltipWrapper>
     </>
   );
-}
+};
 
 export default Tooltip;
-
-

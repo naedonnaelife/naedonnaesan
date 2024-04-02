25f9ae97c95dbe3ba7e82d1b0fc6cf8e7e004ae3
지민 <81950388+jjm6604@users.noreply.github.com>
2024-04-02 10:07:46 +0900

    Fix: build 오류 수정

--- Front/src/components/comparison/DetailGraph.tsx
+++ Front/src/components/comparison/DetailGraph.tsx
@@ -1,14 +1,7 @@
-import React, { useState, useEffect } from "react";
-import tw, { styled } from "twin.macro";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Tooltip,
-  Legend,
-} from "chart.js";
-import { Bar } from "react-chartjs-2";
+import React, { useState, useEffect } from 'react';
+import tw, { styled } from 'twin.macro';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Tooltip, Legend } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
 
 interface DetailGraphProps {
   category: string | null;
@@ -18,11 +11,11 @@ interface DetailGraphProps {
   detail2: any | null;
 }
 
-type Detail = {
-  infraTypeName: string;
-  infraName: string;
-  totalCount: number;
-}
+// type Detail = {
+//   infraTypeName: string;
+//   infraName: string;
+//   totalCount: number;
+// }
 
 const GraphWrapper = styled.figure`
   ${tw`flex-c w-[33%] h-[400px] border-t-2 border-gray m-7 mb-5 pt-5
@@ -33,14 +26,8 @@ const GraphTitle = styled.h1`
   ${tw`flex-c text-2xl`}
 `;
 
-const Graph: React.FC<DetailGraphProps> = ({
-  category,
-  selected1,
-  selected2,
-  detail1,
-  detail2,
-}) => {
-  const [infraList, setInfraList] = useState([])
+const Graph: React.FC<DetailGraphProps> = ({ category, selected1, selected2, detail1, detail2 }) => {
+  const [infraList] = useState([]);
   ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip, Legend);
   // 상태를 사용하여 창 크기 변화 감지
   const [, setWindowSize] = useState({
@@ -48,8 +35,19 @@ const Graph: React.FC<DetailGraphProps> = ({
   });
 
   useEffect(() => {
-    console.log('category : ', category, 'selcted1 : ', selected1, "selected2 : ", selected2, 'detail1 : ', detail1, 'detail2 : ', detail2)
-    
+    console.log(
+      'category : ',
+      category,
+      'selcted1 : ',
+      selected1,
+      'selected2 : ',
+      selected2,
+      'detail1 : ',
+      detail1,
+      'detail2 : ',
+      detail2
+    );
+
     // const detailData1 = detail1.filter((e:Detail) => e.infraTypeName === category)
     // const detailData2 = detail2.filter((e:Detail) => e.infraTypeName === category).map((e:Detail, index:number) => {
     //   return [e.infraName, e.totalCount, detailData1[index].totalCount]
@@ -61,9 +59,9 @@ const Graph: React.FC<DetailGraphProps> = ({
         width: window.innerWidth,
       });
     };
-    window.addEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize);
     // 컴포넌트 언마운트 시 이벤트 리스너 제거
-    return () => window.removeEventListener("resize", handleResize);
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   const options = {
@@ -71,7 +69,7 @@ const Graph: React.FC<DetailGraphProps> = ({
     maintainAspectRatio: false,
     plugins: {
       legend: {
-        position: "top" as const,
+        position: 'top' as const,
       },
     },
     scales: {
@@ -81,42 +79,45 @@ const Graph: React.FC<DetailGraphProps> = ({
     },
   };
 
-  const labels = detail1 ? detail1.reduce((labelArray: string[], detail: any) => {
-    if (detail.infraTypeName === category) {
-      labelArray.push(detail.infraName);
-    }
-    return labelArray;
-  }, []) : [];
+  // const labels = detail1
+  //   ? detail1.reduce((labelArray: string[], detail: any) => {
+  //       if (detail.infraTypeName === category) {
+  //         labelArray.push(detail.infraName);
+  //       }
+  //       return labelArray;
+  //     }, [])
+  //   : [];
 
-  const getData = e => {
-      const test = {
-      labels : e[0],
+  const getData = (e: any) => {
+    const test = {
+      labels: e[0],
       datasets: [
         {
           label: `${selected1}`,
           data: e[1],
-          backgroundColor: "#8EBE6D",
+          backgroundColor: '#8EBE6D',
         },
         {
           label: `${selected2}`,
           data: e[2],
-          backgroundColor: "#FB8D75",
+          backgroundColor: '#FB8D75',
         },
       ],
-    }
-    console.log(test)
-    return test
     };
+    console.log(test);
+    return test;
+  };
 
   return (
     <>
-    <GraphTitle>
+      <GraphTitle>
         {selected1} {selected2} {category} 수 비교
-    </GraphTitle>
+      </GraphTitle>
       <GraphWrapper>
-      {infraList.map((e) => <Bar options={options} data={getData(e)} />)}
-      
-    </GraphWrapper>
+        {infraList.map((e) => (
+          <Bar options={options} data={getData(e)} />
+        ))}
+      </GraphWrapper>
     </>
   );
 };

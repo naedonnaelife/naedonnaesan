00019a25e0766524e9559cd903f6ecf669f0a15c
지민 <81950388+jjm6604@users.noreply.github.com>
2024-04-03 16:14:43 +0900

    Feat: 마이페이지 레포트

--- Front/src/components/my/ReportContent.tsx
+++ Front/src/components/my/ReportContent.tsx
@@ -4,7 +4,12 @@ import { useNavigate } from 'react-router-dom';
 import RecommendList from '../../utils/RecommendList';
 import UseAxios from '../../utils/UseAxios';
 import useSearchStore from '../../stores/SearchStore';
-import face from '../../assets/face1.png';
+import face1 from '../../assets/face1.png';
+import face2 from '../../assets/face2.png';
+import face3 from '../../assets/face3.png';
+import face4 from '../../assets/face4.png';
+import sadFace from '../../assets/sadFace.png';
+// import face6 from '../../assets/face6.png';
 
 interface ReportContentProps {
   name: string | null;
@@ -29,6 +34,9 @@ const PreferenceWrapper = styled.ul`
   max-sm:w-full max-sm:h-[50%]`}
 `;
 
+const EmptyCard = styled.figure`
+  ${tw`flex-cc w-[100%] h-[100%] animate-fade-left animate-duration-500`}
+`;
 const Card = styled.figure`
   ${tw`flex-cc w-[100%] h-[100%]`}
 `;
@@ -39,7 +47,7 @@ const CartTitle = styled.h1`
   ${tw`text-2xl`}
 `;
 const InfraContent = styled.p`
-  ${tw`text-xl text-blue-500`}
+  ${tw`text-xl text-blue-500 text-center`}
 `;
 const CardContent = styled.p`
   ${tw`font-jamsilMedium text-center`}
@@ -51,6 +59,7 @@ const ReportContent: React.FC<ReportContentProps> = ({ name }) => {
   const axios = UseAxios();
   const navigate = useNavigate();
   const areaName = useSearchStore((state) => state.areaName);
+  const update = useSearchStore((state) => state.selectedArea);
   const updateRecommendList = useSearchStore((state) => state.updateRecommendList);
 
   useEffect(() => {
@@ -68,6 +77,7 @@ const ReportContent: React.FC<ReportContentProps> = ({ name }) => {
 
   useEffect(() => {
     if (isEnter) {
+      update('');
       navigate('/information', { state: { areaName: areaName } });
     } else {
       setIsEnter(true);
@@ -100,22 +110,24 @@ const ReportContent: React.FC<ReportContentProps> = ({ name }) => {
     lst.filter((category) => (preferences ? preferences[category] === score : null))
   );
 
-  const cardTitles = ['욕심이 많은', '현실을 아는', '모든게 다 좋은'];
+  const scoreIndex = scoreList[0].length ? Math.min(Math.floor((scoreList[0].length - 1) / 2), 3) : 0;
+  const imageList = [face1, face2, face3, face4];
+  const cardTitles = ['프로 만족러', '프로 타협러', '프로 꼼꼼러', '프로 욕심러'];
 
   return (
     <Backgroud>
       <PreferenceWrapper>
         {preferences === null ? (
-          <Card>
-            <CardImage src={face} alt="face" />
+          <EmptyCard>
+            <CardImage src={sadFace} alt="sad" />
             <CartTitle>{name}님!</CartTitle>
             <CardContent>아직 검사결과가 없어요</CardContent>
-          </Card>
+          </EmptyCard>
         ) : (
           <Card>
-            <CardImage src={face} alt="face" />
+            <CardImage src={imageList[scoreIndex]} alt="face" />
             <CartTitle>
-              {cardTitles[0]} {name}님!
+              {cardTitles[scoreIndex]} {name}님!
             </CartTitle>
             <InfraContent>
               {scoreList[0].map((lst) => (
@@ -123,7 +135,7 @@ const ReportContent: React.FC<ReportContentProps> = ({ name }) => {
               ))}
             </InfraContent>
             <CardContent>
-              항목을 중요하다고 선택했어요.
+              {scoreList[0].length !== 0 ? '항목을 중요하다고 선택했어요.' : '중요한 항목이 없어요.'}
               <br />
               그런 {name}님에게 추천하는 동네는?
             </CardContent>

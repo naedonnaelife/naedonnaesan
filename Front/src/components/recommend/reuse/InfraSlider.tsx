a36a20d62d82f3f5ec7dd88c4b634cf25ccce2da
지민 <81950388+jjm6604@users.noreply.github.com>
2024-04-03 12:09:32 +0900

    Fix: 추천 사이드바 수정

--- Front/src/components/recommend/reuse/InfraSlider.tsx
+++ Front/src/components/recommend/reuse/InfraSlider.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
-import tw, { styled } from "twin.macro";
-import Tooltip from "../../../utils/Tooltip";
+import { useEffect, useState } from 'react';
+import tw, { styled } from 'twin.macro';
+import Tooltip from '../../../utils/Tooltip';
 
 interface SliderProps {
   data: {
@@ -18,7 +18,7 @@ type StyleProps = {
 };
 
 const SelectWrapper = styled.div`
-${tw`flex flex-col h-[33.3%] w-[33.3%]
+  ${tw`flex flex-col h-[33.3%] w-[33.3%]
 max-sm:h-[26%] max-sm:w-[50%]`}
 `;
 const NameWrapper = styled.div`
@@ -49,22 +49,30 @@ const Input = styled.input`
 `;
 
 const ScoreButton = styled.li`
-  ${tw`my-1 cursor-pointer`}
+  ${tw`font-jamsilLight my-1 cursor-pointer`}
 `;
 
-const InfraSlider: React.FC<SliderProps> = ({data, value, changeScore}) => {
-  const [nowValue, setNowValue] = useState(value)
-  const [gradient, setGradient] = useState(`linear-gradient(to right, #FB8D75 0%, #FB8D75 ${(value-1)/2 * 100}%, #e1e1e1 ${(value-1)/2 * 100}%, #e1e1e1 100%)`)
+const InfraSlider: React.FC<SliderProps> = ({ data, value, changeScore }) => {
+  const [nowValue, setNowValue] = useState(value);
+  const [gradient, setGradient] = useState(
+    `linear-gradient(to right, #FB8D75 0%, #FB8D75 ${((value - 1) / 2) * 100}%, #e1e1e1 ${
+      ((value - 1) / 2) * 100
+    }%, #e1e1e1 100%)`
+  );
   const handleValue = (e: number | React.ChangeEvent<HTMLInputElement>) => {
     let intValue = 0;
-    if (typeof e === "number") {
+    if (typeof e === 'number') {
       intValue = e;
     } else {
       intValue = parseInt(e.target.value);
     }
     setNowValue(intValue);
-    setGradient(`linear-gradient(to right, #FB8D75 0%, #FB8D75 ${(intValue- 1)/2 * 100}%, #e1e1e1 ${(intValue- 1)/2 * 100}%, #e1e1e1 100%)`)
-    changeScore(intValue, data.pk)
+    setGradient(
+      `linear-gradient(to right, #FB8D75 0%, #FB8D75 ${((intValue - 1) / 2) * 100}%, #e1e1e1 ${
+        ((intValue - 1) / 2) * 100
+      }%, #e1e1e1 100%)`
+    );
+    changeScore(intValue, data.pk);
   };
 
   useEffect(() => {
@@ -76,14 +84,7 @@ const InfraSlider: React.FC<SliderProps> = ({data, value, changeScore}) => {
       <NameWrapper>
         {data.name} <Tooltip data={data.detail} />
       </NameWrapper>
-      <Input
-        gradient={gradient}
-        type="range"
-        min={1}
-        max={3}
-        value={nowValue}
-        onChange={handleValue}
-      ></Input>
+      <Input gradient={gradient} type="range" min={1} max={3} value={nowValue} onChange={handleValue}></Input>
       <Wrapper>
         <ScoreButton onClick={() => handleValue(1)}>낮음</ScoreButton>
         <ScoreButton onClick={() => handleValue(2)}>보통</ScoreButton>
